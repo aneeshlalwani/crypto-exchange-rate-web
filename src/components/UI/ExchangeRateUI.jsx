adb3e29d1aaf16476fd411ea034a22d793761ff8
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-02-23 20:33:03 +0500

    displayed data in UI

--- src/components/UI/ExchangeRateUI.jsx
+++ src/components/UI/ExchangeRateUI.jsx
@@ -1,6 +1,20 @@
 import { Typography, Card } from "antd";
 
 const ExchangeRateUI = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const { price, dataObj, currencySymbol } = props;
+  // eslint-disable-next-line react/prop-types
+  const toCurrency = dataObj.toCurrency;
+  let value = Number(price);
+  let currencyCode = toCurrency;
+
+  let currency = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: currencyCode,
+  });
+
+  let formattedCurrency = currency.format(value);
+
   return (
     <>
       <div className="exchange-rate-ui">
@@ -10,7 +24,7 @@ const ExchangeRateUI = (props) => {
           style={{ width: "350px", backgroundColor: "#4d4add", color: "#FFF" }}
         >
           <Typography.Paragraph style={{ color: "#FFF" }}>
-            Bitcoin
+            {formattedCurrency}
           </Typography.Paragraph>
         </Card>
       </div>
