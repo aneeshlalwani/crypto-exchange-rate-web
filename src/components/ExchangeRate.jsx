90000e4b72fe46f44ec18bef81153bf4984bac39
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-02-23 18:24:42 +0500

    implemented use query hook

--- src/components/ExchangeRate.jsx
+++ src/components/ExchangeRate.jsx
@@ -2,11 +2,37 @@ import { Typography, Select } from "antd";
 import { cryptoCurrencies, fiatCurrencies } from "./currencies/currencies";
 import ExchangeRateUI from "./UI/ExchangeRateUI";
 import { useState } from "react";
+import { useQuery } from "react-query";
+import { fetchData } from "./fetchData/fetchData.jsx";
+import getExchangeRate from "./fetchData/fetchData";
 
 const ExchangeRate = () => {
   const [fromCurrency, setFromCurrency] = useState(cryptoCurrencies[0].value);
   const [toCurrency, setToCurrency] = useState(fiatCurrencies[0].value);
 
+  const dependencies = {
+    fromCurrency: fromCurrency,
+    toCurrency: toCurrency,
+  };
+  /* Using useQuery hook to fetch the exchange rate data with specified parameters
+     The useQuery hook returns an object with many properties, but I have used those properties
+    that are of interest:
+        data: holds the fetched data.
+        isLoading: indicates if the data is still being fetched.
+        isError: indicates if an error occurred during fetching.
+        error: contains the error details if there's an issue. 
+*/
+  // destructuring object returned by hook
+  const { data, isLoading, isError, error } = useQuery({
+    // defining a unique identifier for the query
+    queryKey: ["exchangeRate", dependencies],
+    /* fetchData function to retrieve the exchange rate between fromCurrency and toCurrency. */
+    queryFn: () => fetchData(fromCurrency, toCurrency),
+    //    This tells react-query to consider the data fresh for 1 minute after it's initially fetched.
+    staleTime: 1000 * 60,
+    retry: 1,
+    retryDelay: 6000,
+  });
   function handleFromCurrencyChange(e) {
     setFromCurrency(e);
     console.log(e);
