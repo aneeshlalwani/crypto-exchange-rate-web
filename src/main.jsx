fe1e24d712ace332d783ebad3f8b0a7749005e5a
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-02-25 13:49:14 +0500

    optimized error handling

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import ErrorBoundary from "./ErrorBoundary";
+import "./App.css";
+import ErrorBoundary from "../src/components/ErrorBoundary";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
